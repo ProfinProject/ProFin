b827a96cf4cc5bacc055b6ec5985148b18669ba3
Breno Francisco Morais <62041808+BREN0-MORAIS@users.noreply.github.com>
2025-01-13 14:55:30 -0400

    Reapply "Criação de repository genérico para facilitar na implementação de CRUD."
    This reverts commit 409c4239920d75173a1b12e99428a0481365d71c.

---
+++ src/ProFin.Core/Business/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProFin.Core.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProFin.Core.Business.Interfaces
+{
+    public interface IRepository<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAll(string includes = null, Expression<Func<TEntity, bool>>? expression = null);
+        Task<TEntity> GetById(int id, string? includes = null, Expression<Func<TEntity, bool>>? expression = null);
+        Task Delete(TEntity entity);
+        Task Update(TEntity entity);
+        Task<TEntity> Add(TEntity entity);
+    }
+}

7fca9a857a76863fb33200323abd146ec7333553
Breno Francisco Morais <62041808+BREN0-MORAIS@users.noreply.github.com>
2025-01-14 10:57:01 -0400

    Criação de entidade Categoria e ViewModel.

---
+++ src/ProFin.API/ViewModel/CategoryTransactionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProFin.API.ViewModel;
+
+public class CategoryTransactionViewModel
+{
+    [Required(ErrorMessage = "The name is mandatory.")]
+    [StringLength(100, ErrorMessage = "The name cannot be longer than 100 characters.")]
+    public string Nome { get; set; }
+
+    [StringLength(250, ErrorMessage = "The description cannot be longer than 250 characters.")]
+    public string? Descricao { get; set; }
+}

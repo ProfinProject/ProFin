a51ff4a80e097746bae04a3c5f0c5baa6782b3a4
Breno Francisco Morais <62041808+BREN0-MORAIS@users.noreply.github.com>
2025-02-12 18:01:14 -0400

    Criação de switch de banco de dados.

--- src/Back-End/ProFin.API/Configurations/DbContextConfig.cs
+++ src/Back-End/ProFin.API/Configurations/DbContextConfig.cs
@@ -1,5 +1,6 @@
 ﻿
 using Microsoft.EntityFrameworkCore;
+using ProFin.Core.Enumeradores;
 using ProFin.Data.Context;
 using ProFin.Data.Seed;
 
@@ -7,12 +8,25 @@ namespace ProFin.API.Configurations
 {
     public static class DbContextConfig
     {
-        public static WebApplicationBuilder AddDbContextConfig(this WebApplicationBuilder builder)
+        public static WebApplicationBuilder AddDbContextConfig(this WebApplicationBuilder builder, EDatabases databases)
         {
-            builder.Services.AddDbContext<AppDbContext>(options =>
+            switch (databases)
             {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
+                case EDatabases.SQLServer:
+                    builder.Services.AddDbContext<AppDbContext>(options =>
+                        options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer"))
+                    );
+                    break;
+
+                case EDatabases.SQLite:
+                    builder.Services.AddDbContext<AppDbContext>(options =>
+                        options.UseSqlite(builder.Configuration.GetConnectionString("SQLite"))
+                    );
+                    break;
+
+                default:
+                    throw new ArgumentException($"Banco de dados {databases} não suportado.");
+            }
 
             builder.Services.AddTransient<DbMigrationHelper>();
 

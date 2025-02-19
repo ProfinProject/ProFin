d72194953b5aaee3b0c2435fe46e72f2080e8c51
Fabiano Marcolin Maciel <fmaciel@glasslewis.com>
2025-02-19 17:46:55 +0000

    adicionando automapperconfig de volta

--- src/Back-End/ProFin.API/Program.cs
+++ src/Back-End/ProFin.API/Program.cs
@@ -18,6 +18,7 @@ internal class Program
             .AddIdentity()
             .AddJwt()
             .AddDbContextConfig(EDatabases.SQLite)
+            .AddAutoMapperConfig()
             .AddDIConfig()
             .AddCorsPolicy();
 

347119420407218e7c0259de5179a86393df7019
Fabiano Marcolin Maciel <fmaciel@glasslewis.com>
2025-02-25 20:36:15 +0000

    melhorando a estrutura de paneis

---
+++ src/Front-End/ProFin.App/src/app/panel/components/panel-detail/panel-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'app-panel-detail',
+  standalone: true,
+  templateUrl: './panel-detail.component.html',
+  styleUrl: './panel-detail.component.css',
+  imports: [RouterLink]
+})
+export class PanelDetailComponent {
+
+}
